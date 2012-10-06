7779b62a57f5b0f39d64546e23f44316270ac6a7
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2012-10-06 13:43:00 -0500

    initial commit 2

---
+++ src/main/resources/TestResourceClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikh0881
+ * Date: 10/6/12
+ * Time: 1:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestResourceClass {
+}
