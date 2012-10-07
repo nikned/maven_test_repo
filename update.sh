d0ed5a92e77a6535f0daa13c91ddaff9d2d8761b
Nikhil Nedunuri <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2012-10-07 18:57:52 -0500

    final one

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git submodule update
-cd src/main/resources/git-contracts
-git checkout master
-git pull
+git submodule update --init #initialized the submodule repository
+cd src/main/resources/git-contracts  #entering into the repo
+git checkout master # checkout the latest from repo
+git pull #pull latest changed into the branch
