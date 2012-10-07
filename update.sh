924420683c455f5fd462b3152270a67eb6d67d3b
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2012-10-06 23:39:25 -0500

    modified the shell script and the pom file

--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
-git submodule update --init --recursive
+#!/bin/bash
+current="$(pwd)"
+echo "'$current'" '->' "'$(pwd)'" #show paths for demo purposes
+git submodule update --init
+git submodule foreach git submodule update --init
+git submodule sync
